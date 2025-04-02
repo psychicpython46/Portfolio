5850d5a32e0b9f2e4030fe12cc5bd37125d9c3d1
psychicpython46 <85611523+psychicpython46@users.noreply.github.com>
2025-04-02 20:11:48 +0200

    Add files via upload

---
+++ backend/php/PHP_API_Database_Connect_CRUD/post/delete.php
@@ -0,0 +1,23 @@
+<?php
+header('Acces-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: DELETE');
+header('Access-control-Allow-Headers: Access-control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With'); //not all needed
+include_once '../../config/Database.php';
+include_once '../../models/Post.php';
+
+$database = new Database();
+$db = $database->Connect();
+
+$post = new Post($db);
+
+$data = json_decode(file_get_contents("php://input"));
+$post->id = $data->id;
+
+if ($post->Delete()) {
+    echo json_encode(array('message' => 'Post Deleted'));
+} else {
+    echo json_encode(array('message' => 'Post NOT Deleted'));
+}
+
+?>
